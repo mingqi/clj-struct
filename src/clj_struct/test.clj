c4f38d255e6cf1e358380928b31b187cf11f1a91
shaomq <shaomq@gmail.com>
2013-10-11 11:16:30 +0800

    add more test cases

--- src/clj_struct/test.clj
+++ src/clj_struct/test.clj
@@ -1,19 +1,6 @@
 (ns clj-struct.test
   )
 
-(defn ^java.lang.String ss [ ^java.lang.String s]
-  (println "input is " s)
-  s
-  ;(.toUpperCase s)
-  )
-
-(defn f []
-  (println "hello, mingqi")
-  (definline m[] (println "xulei"))
-  ;(m)
-  )
-
-(defn -main [ & args]
-  (f)
-;  (m)
+(defn tool-main [ & args]
+  (println "aaa")
   )
